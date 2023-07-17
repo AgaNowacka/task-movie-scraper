0599bd953a49cb98e574a6b69e88bcf7000c162c
Agnieszka Nowacka <agnesnowacka1@gmail.com>
2023-07-17 14:29:38 +0200

    added all urls, added max movies per urls function

--- app.js
+++ app.js
@@ -1,33 +1,49 @@
 import * as cheerio from "cheerio";
 import fetch from "node-fetch";
 
+const urls = [
+  "https://www.filmweb.pl/ranking/vod/netflix/film/2023",
+  "https://www.filmweb.pl/ranking/vod/hbo_max/film/2023",
+  "https://www.filmweb.pl/ranking/vod/canal_plus_manual/film/2023",
+  "https://www.filmweb.pl/ranking/vod/disney/film/2023",
+];
+
+const maxMoviesPerURL = 10;
+
 async function getMovies() {
-    try {
+  try {
+    for (const url of urls) {
+      const response = await fetch(url);
+      const body = await response.text();
+      const $ = cheerio.load(body);
 
-        const netflixResponse = await fetch("https://www.filmweb.pl/ranking/vod/netflix/film/2023");
-        
-
-        const body = await netflixResponse.text();
-        const $ = cheerio.load(body);
+      let moviesCount = 0;
 
         const movies = [];
-        $('.rankingTypeSection__container > .rankingType').map((index, element) => {
 
-            const moviePosition = $(element).find('.rankingType__position').text();
-            const movieTitle = $(element).find('.rankingType__titleWrapper > .rankingType__title').text();
-            const movieRating = $(element).find('.rankingType__rateWrapper > .rankingType__rate > .rankingType__rate--value').text();
+      $('.rankingTypeSection__container > .rankingType').map((index, element) => {
+        if (moviesCount < maxMoviesPerURL) {
+          const moviePosition = $(element).find('.rankingType__position').text();
+          const movieTitle = $(element).find('.rankingType__titleWrapper > .rankingType__title').text();
+          const movieRating = $(element).find('.rankingType__rateWrapper > .rankingType__rate > .rankingType__rate--value').text();
 
-            console.log(movieRating);
+          moviesCount++;
 
+          console.log(movieRating);
+        }
 
-        });
-
-
-
-    } catch (error) {
-        console.log(error);
+        movies.push({
+            moviePosition,
+            movieTitle,
+            movieRating
+        })
+      });
     }
+    // console.log(movies);
 
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 getMovies();
